d89c921492e9c43a23c991029ddc1023232c2907
Matheus Almeida de Abreu <matheus.abreu@Matheuss-MacBook-Air.local>
2020-03-30 02:15:45 -0300

    Configurações iniciais google maps.

--- App/src/app/pages/home/home.page.ts
+++ App/src/app/pages/home/home.page.ts
@@ -1,3 +1,4 @@
+/// <reference types="@types/googlemaps" />
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  private map: google.maps.Map;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.initMap();
+  }
+
+  private initMap() {
+    const mapOptions: google.maps.MapOptions = {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    
+    this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
 
 }
--- App/src/main.ts
+++ App/src/main.ts
@@ -4,10 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-const ENV = environment;
-
-document.write(`<script src="${ENV.MAPS_API_KEY}" async defer></script>`);
-
 if (environment.production) {
   enableProdMode();
 }
