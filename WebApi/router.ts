ec23237990e7c47de8017358eae74597ec3d915d
Matheus Almeida de Abreu <matheus10.abreu@hotmail.com>
2020-05-08 01:21:18 -0300

    adding docker compose

--- WebApi/router.ts
+++ WebApi/router.ts
@@ -11,11 +11,14 @@ const router = Router({
 });
 
 router.all('/webapi/login', async (req, res) => new LoginHandler().singIn(req, res));
-router.all('/webapi/login-facebook', async (req, res) => new LoginHandler().verifyFacebook(req, res))
+router.all('/webapi/login_facebook', async (req, res) => new LoginHandler().verifyFacebook(req, res))
 router.get('/webapi/users', UserHandler.getAll);
 router.get('/webapi/user/:id', UserHandler.getById);
 
 router.get('/webapi/eventos', async (req, res) => await new EventoHandler().getAll(req, res));
+router.get('/webapi/eventos_proximos', (req, res) => {
+    res.send('teste1');
+});
 
 router.get('/webapi/endereco', async (req, res) => await new EnderecoHandler().getAll(req, res));
 
